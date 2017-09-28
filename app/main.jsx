318293ecfa64747256f2d22c2cdae585b782d49f
Blake Hagen <blakehagen@gmail.com>
2017-09-27 20:00:06 -0600

    webpack config and scripts

--- app/main.jsx
+++ app/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import styles from './main.scss';
+
 ReactDOM.render(
-  <div>
-    Hello World! I am working!
+  <div className={styles.container}>
+    Hello World! Does it work? I am awesome!!!!!
+    {console.log('hello there!')}
   </div>, document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const _ = require('lodash');
+const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
@@ -10,21 +12,86 @@ const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   inject: 'body'
 });
 
-module.exports = {
+const useProductionBuild = _.includes(['production', 'development'], process.env.NODE_ENV);
+
+const webpackConfig = {
   entry: [
     './app/main.jsx'
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/server/public',
+    publicPath: '/',
     filename: 'index_bundle.js'
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.(jsx)$/,
+        test: /\.jsx$/,
+        exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [autoprefixer({browsers: ['> 1%', 'last 5 versions', 'Firefox ESR', 'safari >= 4']})]
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        })
       }
     ]
   },
-  plugins: [HTMLWebpackPluginConfig]
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new ExtractTextPlugin("style.css"),
+  ]
 };
+
+if (useProductionBuild) {
+  console.log('useProductionBuild --> ', useProductionBuild);
+
+  webpackConfig.devtool = 'source-map';
+
+  webpackConfig.plugins.push(new UglifyJSPlugin({
+    sourceMap: true,
+    uglifyOptions: {
+      compress: {
+        warnings: false,
+        drop_console: false
+      },
+      output: {
+        comments: false,
+      },
+    }
+  }));
+
+  webpackConfig.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }));
+
+} else {
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  webpackConfig.entry.unshift('webpack-hot-middleware/client?reload=true');
+}
+
+module.exports = webpackConfig;
